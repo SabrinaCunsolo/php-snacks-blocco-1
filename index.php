6c4d21522f96d8895264735990d264c6274230a1
Sabrina <sabrinacunsolo85@gmail.com>
2020-12-15 14:53:47 +0100

    inizio snack 1
    creo array $matches con sotto array con partite di basket

---
+++ index.php
@@ -0,0 +1,12 @@
+PHP Snack 1:
+Creare un array $matches che contiene dei sotto-array, i quali rappresentano delle partite di basket.
+ogni partita avrà le seguenti caratteristiche:
+squadra di casa
+squadra ospite
+punti fatti dalla squadra di casa
+punti fatti dalla squadra ospite
+L'obbiettivo è stampare a schermo tutte le partite utilizzando questo schema:
+[squadra di casa] - [squadra ospite] | [punti squadra di casa] - [punti squadra ospite]
+ad esempio:
+Olimpia Milano - Cantù | 55 - 60
+Nome repo: php-snacks-blocco-1
